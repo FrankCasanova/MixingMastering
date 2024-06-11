2ad6cea9f3867e74dcd932dbdd2ea9dc2c36f340
Frank Casanova <frankcasanova.info@gmail.com>
2024-06-11 14:05:34 +0200

    header animated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
-import React,  {useState} from "react";
+import {useState} from "react";
+//import icons
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+//import assets
 import logo from "../../assets/logo.png";
+// import css
 import "./navbar.css";
+//import motion
+import { motion } from "framer-motion";
+//import variants
+import {fadeIn} from "../../variants.js"
+
+
 
 
 const Navbar = () => {
@@ -12,21 +21,31 @@ const Navbar = () => {
     return (
         <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
+        <motion.div variants={fadeIn('right')}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{once: false, amount: 0.2}}
+
+         className="gpt3__navbar-links_logo">
           <img src={logo} />
-        </div>
-        <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is Mixing and Mastering IA</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Our Services</a></p>
-          <p><a href="#blog">Blog</a></p>
-        </div>
+        </motion.div>
+        <motion.div
+        variants={fadeIn('left')}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{once: false, amount: 0.2}} 
+        className="gpt3__navbar-links_container">
+          <p  className="item"><a href="#home">Home</a></p>
+          <p  className="item"><a href="#wgpt3">What is Mixing and Mastering IA</a></p>
+          <p  className="item"><a href="#possibility">Open AI</a></p>
+          <p  className="item"><a href="#features">Our Services</a></p>
+          <p  className="item"><a href="#blog">Blog</a></p>
+        </motion.div>
       </div>
-            <div className="gpt3__navbar-sign">
+            {/* <div className="gpt3__navbar-sign">
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
-            </div>
+            </div> */}
             <div className="gpt3__navbar-menu">
                 {toggleMenu
                     ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
@@ -41,10 +60,10 @@ const Navbar = () => {
                             <p><a href="#features">Our Services</a></p>
                             <p><a href="#blog">Blog</a></p>
                         </div>
-                        <div className="gpt3__navbar-menu_container-links-sign">
+                        {/* <div className="gpt3__navbar-menu_container-links-sign">
                             <p>Sign in</p>
                             <button type="button">Sign up</button>
-                        </div>
+                        </div> */}
                     </div>
                 )}
             </div>
